05f9c2be4c474cceb8b3dcd7085f2d0fe228d62e
wronghandedryan <wronghandedbarber@gmail.com>
2020-08-12 14:49:38 -0700

    1 file modified

--- src/components/authorContent.js
+++ src/components/authorContent.js
@@ -3,14 +3,16 @@ import NewPost from BlogCreatePost;
 
 
 const authorContent = ({eTitle, data})=>{
-  const upvoteArticle = async  ()=>{
+  const authorContent = async  ()=>{
     return{
+    { authorContent.map(author(
       <div class="authorContent" key="eTitle">
         <h1>
           {{data}}
         </h1>
       </div>
+        )
+      )
     }
-  };
 
-export default autherContent;
+export default autherContent();
