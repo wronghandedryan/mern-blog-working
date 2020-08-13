649062d058d4fa65fd18a8b0d61760d422a76f2a
wronghandedryan <wronghandedbarber@gmail.com>
2020-08-12 18:52:27 -0700

    1 file modified

--- src/pages/BlogCreatePost.js
+++ src/pages/BlogCreatePost.js
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
-
+import axios from 'axios'
 export default function BlogCreatePost(){
 const [ data, setData ] = useState('')
  return(
-    <form onSubmit={(data =[eTitle, e, eName]) => createPost(data)}>
+    <form onSubmit={([eTitle, e, eName] = data) => createPost(data)}>
      <label id="PostTitle">Post Title:
-        <textarea onChange={etitle => setData(eTitle.target.value)} value={data} />
+        <textarea onChange={data => setData(eTitle.target.value)} value={data} />
       </label>
       <label id="PostBody">Post:
-        <textarea onChange={e => setData(e.target.value)} value={data} />
+        <textarea onChange={data => setData(e.target.value)} value={data} />
       </label>
       <label id="PostAuthor">Author:
-      <textarea onChange={eName => setData(eName.target.value)} value={data} />
+      <textarea onChange={data => setData(eName.target.value)} value={data} />
       </label>
 
 
